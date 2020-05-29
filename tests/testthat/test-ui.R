1122f22de9f7db1ea8f7b10053628328c157712f
Akira Hayashi <smxshxishxad@me.com>
2020-05-29 15:25:53 +0900

    :children_crossing: Add generate_sr_weight()
    Resolve #304

--- tests/testthat/test-ui.R
+++ tests/testthat/test-ui.R
@@ -5,3 +5,63 @@ test_that("load_vpa() works", {
   expect_is(load_vpa("../../inst/extdata/vpa.csv"), "list")
 })
 
+
+context("- generate_sr_weight()")
+srdata    <- get.SRdata(load_vpa("../../inst/extdata/res_vpa_pma.rda"),
+                        return.df = TRUE)
+test_that("returns proper length vector", {
+  wt_allyr <- generate_sr_weight(using = "allyear", srdata)
+  expect_is(wt_allyr, "numeric")
+  expect_equal(length(wt_allyr), nrow(srdata))
+
+  wt_specyr <- generate_sr_weight(using = 1982, srdata)
+  expect_is(wt_specyr, "numeric")
+  expect_equal(length(wt_specyr), nrow(srdata))
+
+  wt_removeyr <- generate_sr_weight(n_yrs_to_remove = 1, srdata)
+  expect_is(wt_removeyr, "numeric")
+  expect_equal(length(wt_removeyr), nrow(srdata))
+})
+
+n_years <- nrow(srdata)
+assertthat::assert_that(max(srdata$year) == 2011)
+assertthat::assert_that(n_years == 30)
+
+test_that("sets specific years to one", {
+
+  expect_equal(generate_sr_weight(using = 2011, srdata),
+               c(rep(0, n_years - 1), 1))
+  expect_equal(generate_sr_weight(using = 2010:2011, srdata),
+               c(rep(0, n_years - 2), 1, 1))
+  expect_equal(generate_sr_weight(using = c(2009, 2011), srdata),
+               c(rep(0, n_years - 3), 1, 0, 1))
+  expect_equal(generate_sr_weight(using = 1982:1990, srdata),
+               c(rep(1, 9), rep(0, 21)))
+})
+
+test_that("removes recent years by given number", {
+
+  test_n_years_removed <- function(number_of_years_to_remove) {
+    expect_equal(
+      generate_sr_weight(n_yrs_to_remove = number_of_years_to_remove,
+                    srdata),
+      c(rep(1, n_years - number_of_years_to_remove),
+        rep(0, number_of_years_to_remove)))
+  }
+
+  test_n_years_removed(1)
+  test_n_years_removed(2)
+  test_n_years_removed(3)
+  test_n_years_removed(10)
+
+})
+
+test_that(" returns error for unwanted args", {
+  expect_error(generate_sr_weight(srdata),
+               "Either `using` or `except` should be given")
+  
+  expect_error(generate_sr_weight(using = "allyear",
+                             n_yrs_to_remove = 1,
+                             srdata),
+               "Either `using` or `except` should be given")
+})
